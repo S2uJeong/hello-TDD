7635545e91589d3e078f5fb6e3e6b321e93bed7a
sujeong <sj980204@gmail.com>
2024-07-03 14:38:21 +0900

    [refac] PasswordStrengthMeterTest.java 코드 리팩터링 - PasswordStrengthMeter 클래스 필드 생성자 추가로 중복 코드 제거 - assertStrength() 생성으로 중복 코드 제거

--- src/test/java/chap02/PasswordStrengthMeterTest.java
+++ src/test/java/chap02/PasswordStrengthMeterTest.java
@@ -6,28 +6,28 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class PasswordStrengthMeterTest {
     PasswordStrengthMeter meter = new PasswordStrengthMeter();
+
+    // 입력된 string 비밀번호의 강도를 구하고 그 값이 예상값 파라미터와 같은지 비교
+    private void assertStrength(String password, PasswordStrength expStr) {
+        PasswordStrength result = meter.meter(password);
+        assertEquals(expStr, result);
+    }
     @Test
     @DisplayName("모든 조건을 만족하면 비밀번호 강도는 강함이다.")
     void test1() {
+        assertStrength("abc1@Add", PasswordStrength.STRONG);
+        assertStrength("abc1!Add", PasswordStrength.STRONG);
 
-        PasswordStrength result = meter.meter("ab12!@AB");
-        assertEquals(PasswordStrength.STRONG, result);
-        PasswordStrength result2 = meter.meter("abc1@Add");
-        assertEquals(PasswordStrength.STRONG, result2);
     }
     @Test
     @DisplayName("길이가 8글자 미만이고 나머지 조건은 충족하는 경우 강도는 보통이다.")
     void test2() {
-        PasswordStrengthMeter meter = new PasswordStrengthMeter();
-        PasswordStrength result = meter.meter("ab12@!A");
-        assertEquals(PasswordStrength.NORMAL, result);
+        assertStrength("ab12@!A", PasswordStrength.NORMAL);
     }
 
     @Test
     @DisplayName("숫자를 포함하지 않고 나머지 조건은 충족하는 경우 강도는 보통이다.")
     void test3() {
-        PasswordStrengthMeter meter = new PasswordStrengthMeter();
-        PasswordStrength result = meter.meter("abdfsd@!A");
-        assertEquals(PasswordStrength.NORMAL, result);
+        assertStrength("abdfsd@!A", PasswordStrength.NORMAL);
     }
 }
