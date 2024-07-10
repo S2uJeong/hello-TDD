8069570cc4a9249c6a95d3b1eadddcecd26e573c
sujeong <sj980204@gmail.com>
2024-07-10 13:39:31 +0900

    [feat] #3 테스트코드 중복 제거

--- src/test/java/chap03/ExpiryDateCalculatorTest.java
+++ src/test/java/chap03/ExpiryDateCalculatorTest.java
@@ -12,14 +12,19 @@ public class ExpiryDateCalculatorTest {
     @Test
     @DisplayName("만원 납부하면 한달 뒤가 만료일이 됨")
     void test1() {
-        LocalDate billingDate = LocalDate.of(2019,01,03);
-        int payAmount = 10_000;
-
-        ExpiryDateCalculator cal = new ExpiryDateCalculator();
-        LocalDate expiryDate = cal.calculateExpiryDate(billingDate, payAmount);
-
-        assertEquals(expiryDate, LocalDate.of(2019,02,03));
+        assertExpiryDate(
+                LocalDate.of(2019,01,02)
+                ,10_000
+                , LocalDate.of(2019,02,02));
+        assertExpiryDate(
+                LocalDate.of(2019,05,02)
+                ,10_000
+                , LocalDate.of(2019,06,02));
     }
-
+    private void assertExpiryDate(LocalDate billingDate, int payAmount, LocalDate expectedExpiryDate) {
+        ExpiryDateCalculator cal = new ExpiryDateCalculator();
+        LocalDate realExpiryDate = cal.calculateExpiryDate(billingDate, payAmount);
+        assertEquals(expectedExpiryDate, realExpiryDate);
+    }
 
 }
