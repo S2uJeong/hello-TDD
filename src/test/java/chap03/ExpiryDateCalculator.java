80b19567db5686a479e335849b5c9e6cfcf77258
sujeong <sj980204@gmail.com>
2024-07-13 11:08:29 +0900

    [feat] 일반화 진행 후

--- src/test/java/chap03/ExpiryDateCalculator.java
+++ src/test/java/chap03/ExpiryDateCalculator.java
@@ -7,9 +7,13 @@ public class ExpiryDateCalculator {
         // return LocalDate.of(2019,02,03);
         // return billingDate.plusMonths(1);
         if (data.getFirstBillingDate() != null) {
-            if (data.getFirstBillingDate().equals(LocalDate.of(2019, 1, 31))) {
-                return LocalDate.of(2019, 3, 31);
+            LocalDate candidateExp = data.getBillingDate().plusMonths(1);
+            if (data.getFirstBillingDate().getDayOfMonth() != candidateExp.getDayOfMonth()) {
+                return candidateExp.withDayOfMonth(data.getFirstBillingDate().getDayOfMonth());
             }
+//            if (data.getFirstBillingDate().equals(LocalDate.of(2019, 1, 31))) {
+//                return LocalDate.of(2019, 3, 31);
+//            }
         }
         return data.getBillingDate().plusMonths(1);
 
