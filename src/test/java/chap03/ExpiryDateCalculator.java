2c608ffd08bd81b43830bacb18dbc7fe4395305b
sujeong <sj980204@gmail.com>
2024-07-13 11:19:55 +0900

    [refac] 후보 만료일이 속한 월의 마지막 일자를 구하는 코드 중복 제거

--- src/test/java/chap03/ExpiryDateCalculator.java
+++ src/test/java/chap03/ExpiryDateCalculator.java
@@ -6,19 +6,20 @@ import java.time.YearMonth;
 public class ExpiryDateCalculator {
     LocalDate calculateExpiryDate(PayData data) {
         int addedMonths = data.getPayAmount() / 10_000;
-        // return LocalDate.of(2019,02,03);
-        // return billingDate.plusMonths(1);
+
         if (data.getFirstBillingDate() != null) {
             LocalDate candidateExp = data.getBillingDate().plusMonths(addedMonths);
             if (data.getFirstBillingDate().getDayOfMonth() != candidateExp.getDayOfMonth()) {
-                if (YearMonth.from(candidateExp).lengthOfMonth() < data.getFirstBillingDate().getDayOfMonth()) {
-                    return candidateExp.withDayOfMonth(YearMonth.from(candidateExp).lengthOfMonth());
+
+                final int dayLenOfCandiMon = YearMonth.from(candidateExp).lengthOfMonth(); // 후보 만료일이 속한 월의 마지막 일자
+
+
+                if ( dayLenOfCandiMon < data.getFirstBillingDate().getDayOfMonth()) {
+                    return candidateExp.withDayOfMonth(dayLenOfCandiMon);
                 }
                 return candidateExp.withDayOfMonth(data.getFirstBillingDate().getDayOfMonth());
             }
-//            if (data.getFirstBillingDate().equals(LocalDate.of(2019, 1, 31))) {
-//                return LocalDate.of(2019, 3, 31);
-//            }
+
         }
         return data.getBillingDate().plusMonths(addedMonths);
 
