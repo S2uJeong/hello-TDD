218ca6b50ae7ce48a61ac706d9f2a4f0620a1ca6
sujeong <sj980204@gmail.com>
2024-07-13 11:45:24 +0900

    [refac] 복잡한 로직 메서드로 분리

--- src/test/java/chap03/ExpiryDateCalculator.java
+++ src/test/java/chap03/ExpiryDateCalculator.java
@@ -6,23 +6,32 @@ import java.time.YearMonth;
 public class ExpiryDateCalculator {
     LocalDate calculateExpiryDate(PayData data) {
         int addedMonths = data.getPayAmount() / 10_000;
-
         if (data.getFirstBillingDate() != null) {
-            LocalDate candidateExp = data.getBillingDate().plusMonths(addedMonths);
-            final int dayOfFirstBilling = data.getFirstBillingDate().getDayOfMonth();
-            if (dayOfFirstBilling != candidateExp.getDayOfMonth()) {
-
-                final int dayLenOfCandiMon = YearMonth.from(candidateExp).lengthOfMonth(); // 후보 만료일이 속한 월의 마지막 일자
-
-
-                if ( dayLenOfCandiMon <dayOfFirstBilling) {
-                    return candidateExp.withDayOfMonth(dayLenOfCandiMon);
-                }
-                return candidateExp.withDayOfMonth(dayOfFirstBilling);
-            }
-
+            return expiryDateUsingFirstBillingDate(data, addedMonths);
+        } else {
+            return data.getBillingDate().plusMonths(addedMonths);
         }
-        return data.getBillingDate().plusMonths(addedMonths);
+    }
+    private LocalDate expiryDateUsingFirstBillingDate(PayData data, int addedMonths) {
+        LocalDate candidateExp = data.getBillingDate().plusMonths(addedMonths);
 
+        if (isSameDayOfMonth(data.getBillingDate(), candidateExp)) {
+            final int dayOfFirstBilling = data.getFirstBillingDate().getDayOfMonth();
+            final int dayLenOfCandiMon = lastDayOfMonth(candidateExp); // 후보 만료일이 속한 월의 마지막 일자
+            if ( dayLenOfCandiMon < dayOfFirstBilling) {
+                return candidateExp.withDayOfMonth(dayLenOfCandiMon);
+            }
+            return candidateExp.withDayOfMonth(dayOfFirstBilling);
+        } else {
+            return candidateExp;
+        }
+    }
+
+    private boolean isSameDayOfMonth(LocalDate date1, LocalDate date2) {
+        return date1.getDayOfMonth() == date2.getDayOfMonth();
+    }
+
+    private int lastDayOfMonth(LocalDate date) {
+        return YearMonth.from(date).lengthOfMonth();
     }
 }
