23b4f42ea3edc0874e6071d0498cf2288c3e2a14
sujeong <sj980204@gmail.com>
2024-12-28 23:28:24 +0900

    [feat] move switch statement to calculator and write testcode

--- src/main/java/testCodeLecture/sample/SampleApplication.java
+++ src/main/java/testCodeLecture/sample/SampleApplication.java
@@ -12,13 +12,7 @@ public class SampleApplication {
         long num1 = Long.parseLong(parts[0]);
         long num2 = Long.parseLong(parts[2]);
         String operator = parts[1];
-        long answer = switch (operator) {
-            case "+" -> num1 + num2;
-            case "-" -> num1 - num2;
-            case "*" -> num1 * num2;
-            case "/" -> num1 / num2;
-            default -> throw new InvaildOperatorException();
-        };
+        long answer = new Calculator().calculate(num1, num2, operator);
         System.out.println(answer);
 
     }
---
+++ src/test/java/testCodeLecture/sample/CalculatorTest.java
@@ -0,0 +1,82 @@
+package testCodeLecture.sample;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+    @Test
+    public void 덧셈_연산을_할_수_있다() {
+        // given
+        long num1 = 2;
+        String operator = "+";
+        long num2 = 3;
+        Calculator calculator = new Calculator();
+
+        //when
+        long result = calculator.calculate(num1, num2, operator);
+
+        //then
+        assertEquals(5, result);
+    }
+
+    @Test
+    public void 뺄셈_연산을_할_수_있다() {
+        // given
+        long num1 = 2;
+        String operator = "-";
+        long num2 = 3;
+        Calculator calculator = new Calculator();
+
+        //when
+        long result = calculator.calculate(num1, num2, operator);
+
+        //then
+        assertEquals(-1, result);
+    }
+
+    @Test
+    public void 나눗셈_연산을_할_수_있다() {
+        // given
+        long num1 = 6;
+        String operator = "/";
+        long num2 = 3;
+        Calculator calculator = new Calculator();
+
+        //when
+        long result = calculator.calculate(num1, num2, operator);
+
+        //then
+        assertEquals(2, result);
+    }
+
+    @Test
+    public void 곱셈_연산을_할_수_있다() {
+        // given
+        long num1 = 2;
+        String operator = "*";
+        long num2 = 3;
+        Calculator calculator = new Calculator();
+
+        //when
+        long result = calculator.calculate(num1, num2, operator);
+
+        //then
+        assertEquals(6, result);
+    }
+
+    @Test
+    public void 잘못된_연산자가_요청으로_들어올_경우_에러가_난다() {
+        // given
+        long num1 = 2;
+        String operator = "x";
+        long num2 = 3;
+        Calculator calculator = new Calculator();
+
+        //when
+        //then
+        assertThrows(InvaildOperatorException.class, () -> {
+            calculator.calculate(num1, num2, operator);
+        });
+    }
+}
