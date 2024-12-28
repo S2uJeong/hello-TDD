9b3baf013126289a5cd99e2f4c3c760d1a2a10cb
sujeong <sj980204@gmail.com>
2024-12-28 23:35:50 +0900

    [feat] move Request read statement to CalculationRequestReader and write testcode

---
+++ src/main/java/testCodeLecture/sample/CalculationRequestReader.java
@@ -0,0 +1,12 @@
+package testCodeLecture.sample;
+
+import java.util.Scanner;
+
+public class CalculationRequestReader {
+    public String[] read() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter two numbers and an operator (e.g 1 + 2) : ");
+        String result = scanner.nextLine();
+        return result.split(" ");
+    }
+}
